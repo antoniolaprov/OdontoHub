12807c521f0c35a2992b3f1257991e547d0900a9
antoniolaprov <oi.antonio2@gmail.com>
2026-04-21 23:19:31 -0300

    [FEAT]-subindo projeto, commit inicial

---
+++ odontohub/src/main/java/com/g4/odontohub/OdontohubApplication.java
@@ -0,0 +1,13 @@
+package com.g4.odontohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontohubApplication.class, args);
+	}
+
+}
---
+++ odontohub/src/test/java/com/g4/odontohub/OdontohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g4.odontohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
